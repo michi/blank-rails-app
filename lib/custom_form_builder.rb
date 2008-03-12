37f98b8b981c8896c02028b368af95cb34fc7944
August Lilleaas <augustlilleaas@gmail.com>
2008-03-12 15:24:25 +0100

    Removed evul debug code from the form builder.

--- lib/custom_form_builder.rb
+++ lib/custom_form_builder.rb
@@ -1,5 +1,4 @@
 class CustomFormBuilder< ActionView::Helpers::FormBuilder
-  DEFAULT_OPTIONS = {}
   helpers = field_helpers + 
     %w(date_select datetime_select time_select) + 
     %w(collection_select select country_select time_zone_select) - 
@@ -8,12 +7,12 @@ class CustomFormBuilder< ActionView::Helpers::FormBuilder
   helpers.each do |name| 
     define_method(name) do |field, *args| 
       options = args.extract_options!
-      label_text = options.delete(:label_text)
+      label_text = options.delete(:label)
       
       if label_text == false
         super(field, options)
       else
-        @template.content_tag(:p, label((label_text || field)) + "<br/>" + super(field, options)) 
+        @template.content_tag(:p, label((label_text || field)) + @template.tag(:br) + super(field, options)) 
       end
     end 
   end
