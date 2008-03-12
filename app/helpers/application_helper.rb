a61c22f5811f40dbd90d0e588873105c35597f44
August Lilleaas <augustlilleaas@gmail.com>
2008-03-12 14:18:38 +0100

    Adding some defaults

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
-# Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  # Dynamically showing all flashes
+  def flashes
+    flash.map {|type, content| content_tag(:div, content, :class => "flash", :id => "flash_#{type}")}
+  end
+  
+  # Make HTML lists out of arrays.
+  def list(items)
+    content_tag(:ul, items.map{|item| content_tag(:li, yield(item))})
+  end
 end
