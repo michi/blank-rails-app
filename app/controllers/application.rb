39176ec43d49f8c21af07d79c3d6396a4ea82424
August Lilleaas <augustlilleaas@gmail.com>
2008-03-12 14:15:03 +0100

    Added blank rails app

---
+++ app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '7f18e54d44448f76102fda61b7f76df6'
+end
