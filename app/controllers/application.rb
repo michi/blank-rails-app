68eacf43b2ba3893c864e8cb9f3ec60c03d1d79e
August Lilleaas <augustlilleaas@gmail.com>
2008-07-12 08:03:51 +0200

    Updated the handle_invalid_record method to use methods for error handling (one for new_record? and one for existing records), to allow overriding these behaviours per-controller (by overriding these new methods)

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -9,16 +9,18 @@ class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordInvalid, :with => :handle_invalid_record
   
   def handle_invalid_record(exception)
-    record = exception.record
-    
-    if record.new_record?
-      respond_to do |format|
-        format.html { render :action => ["new", "index"].detect {|a| action_methods.include?(a) } }
-      end
-    else
-      respond_to do |format| 
-        format.html { render :action => "edit" }
-      end
+    exception.record.new_record? ? fallback_on_invalid_new_record : fallback_on_invalid_existing_record
+  end
+  
+  def fallback_on_invalid_new_record
+    respond_to do |format|
+      format.html { render :action => ["new", "index"].detect {|a| action_methods.include?(a) } }
+    end    
+  end
+  
+  def fallback_on_invalid_existing_record
+    respond_to do |format| 
+      format.html { render :action => "edit" }
     end
   end
 end
