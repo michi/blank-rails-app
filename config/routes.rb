d14e3bc0a3645d10da0bee46b73cbea1a2df7c02
August Lilleaas <augustlilleaas@gmail.com>
2008-07-12 07:25:02 +0200

    Moved the ':page' route to the bottom of the routes file, to avoid conflicts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
+  map.root :controller => "pages"
+  
   map.login 'login', :controller => "sessions", :action => "new"
   map.logout 'logout', :controller => "sessions", :action => "destroy"
-  
   map.resource :profile, :controller => "profile", :member => {:forgot_password => :get, :reset_password => :put}
-  
-  map.root :controller => "pages"
-  map.page ':action', :controller => 'pages'
-  
+    
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+  
+  map.page ':action', :controller => 'pages'
 end
