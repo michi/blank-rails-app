eb3892ef4979fdf240d2b54119faff1bbe277008
August Lilleaas <augustlilleaas@gmail.com>
2008-07-14 14:30:18 +0200

    Using the new integration test dsl stuff on the password reset test.

--- test/integration/password_reset_test.rb
+++ test/integration/password_reset_test.rb
@@ -2,43 +2,38 @@ require "#{File.dirname(__FILE__)}/../test_helper"
 
 class PasswordResetTest < ActionController::IntegrationTest
   def test_random_stuff
-    dude = session_dude
-    dude.visits_index
-    dude.shows_login
-    dude.fails_to_log_in
-    dude.asks_for_password_reset
+    new_session :with => DudeDSL do
+      visits_index
+      shows_login
+      fails_to_log_in
+      asks_for_password_reset
+    end
   end
   
-  private
-  
-  def session_dude
-    open_session do |session|
-      class << session
-        def visits_index
-          get "/"
-          assert_response :success
-          assert_template "pages/index"
-        end
-        
-        def shows_login
-          get "/login"
-          assert_response :success
-        end
-        
-        def fails_to_log_in
-          post "/sessions/create", :user => {:username => "august", :password => "wrooong"}
-          assert_template 'sessions/new'
-        end
-        
-        def asks_for_password_reset
-          user = users(:august)
-          old_password = user.password_hash.dup
-          
-          put "/profile/reset_password", :user => {:identification => 'august'}
-          assert_response :redirect
-          assert_not_equal old_password, user.reload.password_hash
-        end
-      end
+  module DudeDSL
+    def visits_index
+      get "/"
+      assert_response :success
+      assert_template "pages/index"
+    end
+    
+    def shows_login
+      get "/login"
+      assert_response :success
+    end
+    
+    def fails_to_log_in
+      post "/sessions/create", :user => {:username => "august", :password => "wrooong"}
+      assert_template 'sessions/new'
+    end
+    
+    def asks_for_password_reset
+      user = users(:august)
+      old_password = user.password_hash.dup
+      
+      put "/profile/reset_password", :user => {:identification => 'august'}
+      assert_response :redirect
+      assert_not_equal old_password, user.reload.password_hash
     end
   end
 end
